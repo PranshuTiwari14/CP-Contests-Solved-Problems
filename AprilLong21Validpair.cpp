adb963d9ff670ce53498b0ce5c6475e7099f0634
Pranshu Tiwari <78229182+PranshuTiwari14@users.noreply.github.com>
2021-04-05 23:17:43 +0530

    Add files via upload

---
+++ AprilLong21Validpair.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b||b==c||c==a){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
