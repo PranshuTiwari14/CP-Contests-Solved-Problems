52066f591b5375945be3150b13d51b11098a4552
Pranshu Tiwari <78229182+PranshuTiwari14@users.noreply.github.com>
2021-04-28 12:46:26 +0530

    Update EvenSum_Codechef.cpp

--- EvenSum_Codechef.cpp
+++ EvenSum_Codechef.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ll t;
+    cin>>t;
+    for(ll i=1;i<=t;i++){
+        ll n;
+        cin>>n;
+        ll a[n],sum=0;
+        for(int j=0;j<n;j++){
+            cin>>a[j];
+            sum+=a[j];
+        }
+        if(sum%2==0){
+            cout<<1<<endl;
+        }
+        else{
+            cout<<2<<endl;
+        }
 
+    }
+    return 0;
+}
