10c04eb2a6a15401b7577931839f7170a2a971ad
Pranshu Tiwari <78229182+PranshuTiwari14@users.noreply.github.com>
2021-06-01 22:21:29 +0530

    Add files via upload

---
+++ MayLunch21Hoops.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ll t;
+    cin>>t;
+    for(ll i=1;i<=t;i++){
+        ll n;
+        cin>>n;
+        int ans=n/2 +1;
+        cout<<ans<<endl;
+
+    }
+    return 0;
+}
