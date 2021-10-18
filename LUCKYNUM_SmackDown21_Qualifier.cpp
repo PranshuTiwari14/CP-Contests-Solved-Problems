f1339acee2606edaab86cdf0544e8f35768770ff
Pranshu Tiwari <78229182+PranshuTiwari14@users.noreply.github.com>
2021-10-18 23:29:14 +0530

    Add files via upload

---
+++ LUCKYNUM_SmackDown21_Qualifier.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--) {
+	    int a, b, c;
+	    cin >> a >> b >> c;
+	    if(a == 7 || b == 7 || c == 7) {
+	        cout <<  "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
